6a193d5be0384b4bd433c981ff26d975fa7879a0
Postmodern <postmodern.mod3@gmail.com>
2022-01-29 14:30:38 -0800

    Changed the project name to ronin-fuzzer.

--- lib/ronin/fuzzing.rb
+++ lib/ronin/fuzzing.rb
@@ -1,20 +1,22 @@
 #
+# ronin-fuzzer - A Ruby library for generating, mutating, and fuzzing data.
+#
 # Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
-# This file is part of ronin-support.
+# This file is part of ronin-fuzzer.
 #
-# ronin-support is free software: you can redistribute it and/or modify
+# ronin-fuzzer is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published
 # by the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# ronin-support is distributed in the hope that it will be useful,
+# ronin-fuzzer is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with ronin-support.  If not, see <https://www.gnu.org/licenses/>.
+# along with ronin-fuzzer.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 require 'ronin/fuzzing/fuzzer'
